6e3ca50f5a7fb6d2ec4c14e6aa9e83e7ac57c183
dai-1234-spec <longdaibui@gmail.com>
2025-06-11 10:04:11 +0700

    app.js change when doing membership page

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ import 'react-toastify/dist/ReactToastify.css';
 import HomePage from './page/homepage/HomePage';
 import Login from './page/authentication/login/Login';
 import GoogleCallback from './page/authentication/Google-CallBack/google-callback';
-import SignUp from "./page/authentication/signup/SignUp";
+// import SignUp from "./page/authentication/signup/SignUp";
 import PropertyDetails from './page/propertydetails/PropertyDetails'; // Cập nhật đường dẫn
 import Dashboard from './page/dashboardadmin/Dashboard';
 import SellerDashboard from './page/sellerdashboard/SellerDashboard';
+import Membership from './page/membership/Membership';
 
  // Đảm bảo đường dẫn đúng theo cấu trúc dự án
 
@@ -36,10 +37,11 @@ function App() {
                     <Route path="/" element={<HomePage />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/google-callback" element={<GoogleCallback />} />
-                    <Route path="/signup" element={<SignUp />} />
+                    {/* <Route path="/signup" element={<SignUp />} /> */}
                     <Route path="/property/:id" element={<PropertyDetails />} /> {/* Route cho PropertyDetails */}
                     <Route path='/dashboard' element={<Dashboard />} />
                     <Route path='/sellerdashboard' element={<SellerDashboard />} />
+                    <Route path='/membership' element={<Membership/>}/>
 
                     {/* Mở rộng sau:
           <Route path="/signup" element={<Signup />} />
